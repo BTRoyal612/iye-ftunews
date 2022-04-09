cb9cc09b7521c0c5f135b2624fc40a55475991b5
Gia Bao Hoang <gb.hoang02@gmail.com>
2022-04-09 13:29:41 +0930

    update margin, layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
-$(function () {
-
+$(function() {
+    
     "use strict";
-
+    
     //===== Prealoder
-
+    
     // $(window).on('load', function(event) {
     //     $('.preloader').delay(500).fadeOut(500);
     // });
-
-
+    
+    
     //===== Sticky
 
     $(window).on('scroll', function (event) {
@@ -37,8 +37,8 @@ $(function () {
             }
         });
     });
-
-
+    
+    
     //===== close navbar-collapse when a clicked
 
     $(".navbar-nav a").on('click', function () {
@@ -52,8 +52,8 @@ $(function () {
     $(".navbar-nav a").on('click', function () {
         $(".navbar-toggler").removeClass('active');
     });
-
-
+    
+    
     //===== Sidebar
 
     $('[href="#side-menu-left"], .overlay-left').on('click', function (event) {
@@ -63,75 +63,46 @@ $(function () {
     $('[href="#close"], .overlay-left').on('click', function (event) {
         $('.sidebar-left, .overlay-left').removeClass('open');
     });
-
-
-
+    
+    
+    
     //===== Back to top
-
+    
     // Show or hide the sticky footer button
-    $(window).on('scroll', function (event) {
-        if ($(this).scrollTop() > 600) {
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
             $('.back-to-top').fadeIn(100)
-        } else {
+        } else{
             $('.back-to-top').fadeOut(100)
         }
     });
-
-
+    
+    
     //Animate the scroll to yop
-    $('.back-to-top').on('click', function (event) {
+    $('.back-to-top').on('click', function(event) {
         event.preventDefault();
-
+        
         $('html, body').animate({
             scrollTop: 0,
         }, 100);
     });
-
-
+    
+    
     //===== 
-
-    var countDownDate = new Date("April 24, 2022 20:00:00").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        document.getElementById("day").firstElementChild.innerHTML = days;
-        document.getElementById("hour").firstElementChild.innerHTML = hours;
-        document.getElementById("minute").firstElementChild.innerHTML = minutes;
-        document.getElementById("second").firstElementChild.innerHTML = seconds;
-        // If the count down is finished, set all to 00
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("day").firstElementChild.innerHTML = "00";
-            document.getElementById("hour").firstElementChild.innerHTML = "00";
-            document.getElementById("minute").firstElementChild.innerHTML = "00";
-            document.getElementById("second").firstElementChild.innerHTML = "00";
-        }
-    }, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
